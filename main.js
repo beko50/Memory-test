5c8f2b2041b784f6c74aaf5f7d9e97773f5d393d
beko50 <essiamah20@gmail.com>
2023-08-09 14:01:46 +0000

    Increasing memory time

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ function startGame() {
             cards.forEach(card => {
                 card.firstElementChild.classList.remove("visible");
             })
-        }, 2000);
+        }, 3000);
         startTimer();
     }
 };
